4f771d98b8c87e0adb5d9c9d4f0e5b8f713c74b4
Luis Hong <luishong50@gmail.com>
2019-03-30 02:58:17 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -40,8 +40,8 @@ def monopoly_with_third_tier
 	    
 	              },
 	    :rent_in_dollars => {:one_piece_owned => 25,
-	                         :two_piece_owened => 50,
-	                         :three_piece_owened => 100,
+	                         :two_pieces_owened => 50,
+	                         :three_pieces_owened => 100,
 	                         :four_pieces_owened => 200
 	    }
 	    
